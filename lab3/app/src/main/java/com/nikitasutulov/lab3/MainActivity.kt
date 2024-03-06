6908dc76ab7f9bc827b9db97f83c414f7392c8b3
Nikita Sutulov <notsid212@gmail.com>
2024-03-06 11:26:36 +0200

    lab3 removed redundant comments

--- lab3/app/src/main/java/com/nikitasutulov/lab3/MainActivity.kt
+++ lab3/app/src/main/java/com/nikitasutulov/lab3/MainActivity.kt
@@ -10,13 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Begin the transaction
         val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
-        // Replace the contents of the container with the new fragment
         ft.replace(R.id.fragment1_layout, InputFragment())
         ft.replace(R.id.fragment2_layout, OutputFragment())
-        // or ft.add(R.id.your_placeholder, new FooFragment());
-        // Complete the changes added above
         ft.commit()
     }
 }
